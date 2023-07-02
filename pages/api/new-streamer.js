dee07264d92606e6d1bf9c0d4fbc2f583387d4e2
starrkTheDev <115180141+starrkTheDev@users.noreply.github.com>
2023-07-02 20:26:58 +0200

    Update new-streamer.js

--- pages/api/new-streamer.js
+++ pages/api/new-streamer.js
@@ -4,7 +4,7 @@ async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
 
-        const client = await MongoClient.connect('mongodb+srv://admin333:sl4b33lo@streamercluster.tglmjqw.mongodb.net/streamers?retryWrites=true&w=majority')
+        const client = await MongoClient.connect('mongodb+srv://admin333:********@streamercluster.tglmjqw.mongodb.net/streamers?retryWrites=true&w=majority')
 
         const db = client.db();
         const streamersCollection = db.collection('streamers');
